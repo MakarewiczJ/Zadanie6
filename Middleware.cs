beb584f7c70e8782e748fbe37ad45d15bf476304
Jakub Makarewicz <Makaro0on23@gmail.com>
2021-04-26 23:50:19 +0200

    komunikat

---
+++ Middleware.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zadanie6
+{
+    public class Middleware
+    {
+        private RequestDelegate _next;
+        public Middleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public Task Invoke(HttpContext context)
+        {
+            var request = context.Request.Headers["User-Agent"];
+            if(request.ToString().ToLower().Contains("edg") || request.ToString().ToLower().Contains("trident"))
+            {
+                context.Response.WriteAsync("<p>Przegladarka nie jest obslugiwana</p>");
+            }
+            return _next(context);
+        }
+    }
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<Middleware>();
+        }
+    }
+}
